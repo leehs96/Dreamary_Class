b0ba6ee0e53ab8776b27754c0f65e5bc347b03b0
Lee Han Sai <hs960201@gmail.com>
2020-08-25 14:25:43 +0900

    완강

--- page/urls.py
+++ page/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = "home"),
-    path('introduce/', views.introduce, name ="introduce"),
-    path('profile/<int:designer_id>', views.detail, name = "detail"),
+    path('introduce/', views.introduce, name = "introduce"),
+    path('profile/<int:designer_id>/', views.detail, name = "detail"),
+    path('new/', views.new, name = "new"),
+    path('create/', views.create, name="create"),
+    path('update/<int:designer_id>/', views.update, name="update"),
 ]
